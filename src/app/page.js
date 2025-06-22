395803b6ff0f6131d15def094e55520c0574d300
Arham Malik <arhammalik900@yahoo.com>
2025-06-22 19:39:03 +0500

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="container mx-auto px-4 py-8 h-screen">
+<main className="container mx-auto px-4 py-8 h-[90%]">
       <Header/>
       <VideoFeed videos={videos} />
     </main>
